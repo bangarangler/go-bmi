363d91f2e5557f09ffd8686dbce2694ece0f6a11
Jon <bangarangler@gmail.com>
2021-06-23 11:26:23 -0400

    small changes to work for us vs metric

--- bmi.go
+++ bmi.go
@@ -11,28 +11,61 @@ import (
 
 func main() {
 	// Output information
+	fmt.Print(info.MesurmentType)
+	mesurmentType, _ := reader.ReadString('\n')
+	if runtime.GOOS == "windows" {
+		mesurmentType = strings.Replace(mesurmentType, "\r\n", "", -1)
+		mesurmentType = strings.Replace(mesurmentType, "\r\n", "", -1)
+	} else {
+		mesurmentType = strings.Replace(mesurmentType, "\n", "", -1)
+		mesurmentType = strings.Replace(mesurmentType, "\n", "", -1)
+	}
 	fmt.Println(info.MainTitle)
 	fmt.Println(info.Seperator)
 
 	// prompt for user input (weight + height)
-	fmt.Print(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n') // read until the user hits enter
+	switch mesurmentType {
+	case "metric":
+		fmt.Print(info.WeightPromptM)
+		weightInput, _ := reader.ReadString('\n') // read until the user hits enter
 
-	fmt.Print(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n') // read until the user hits enter
+		fmt.Print(info.HeightPromptM)
+		heightInput, _ := reader.ReadString('\n') // read until the user hits enter
 
-	// save user input in variables
-	if runtime.GOOS == "windows" {
-		weightInput = strings.Replace(weightInput, "\r\n", "", -1)
-		heightInput = strings.Replace(heightInput, "\r\n", "", -1)
-	} else {
-		weightInput = strings.Replace(weightInput, "\n", "", -1)
-		heightInput = strings.Replace(heightInput, "\n", "", -1)
+		// save user input in variables
+		if runtime.GOOS == "windows" {
+			weightInput = strings.Replace(weightInput, "\r\n", "", -1)
+			heightInput = strings.Replace(heightInput, "\r\n", "", -1)
+		} else {
+			weightInput = strings.Replace(weightInput, "\n", "", -1)
+			heightInput = strings.Replace(heightInput, "\n", "", -1)
+		}
+		weight, _ := strconv.ParseFloat(weightInput, 64)
+		height, _ := strconv.ParseFloat(heightInput, 64)
+		// calculate BMI (weight / height * height)
+		bmi := weight / (height * height)
+		// output the calculated BMI
+		fmt.Printf("Your BMI: %.2f", bmi)
+	case "english":
+		fmt.Print(info.WeightPromptE)
+		weightInput, _ := reader.ReadString('\n') // read until the user hits enter
+
+		fmt.Print(info.HeightPromptE)
+		heightInput, _ := reader.ReadString('\n') // read until the user hits enter
+
+		// save user input in variables
+		if runtime.GOOS == "windows" {
+			weightInput = strings.Replace(weightInput, "\r\n", "", -1)
+			heightInput = strings.Replace(heightInput, "\r\n", "", -1)
+		} else {
+			weightInput = strings.Replace(weightInput, "\n", "", -1)
+			heightInput = strings.Replace(heightInput, "\n", "", -1)
+		}
+		weight, _ := strconv.ParseFloat(weightInput, 64)
+		height, _ := strconv.ParseFloat(heightInput, 64)
+		// calculate BMI (weight / height * height)
+		bmi := (weight / (height * height)) * 703
+		// output the calculated BMI
+		fmt.Printf("Your BMI: %.2f", bmi)
 	}
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
-	// calculate BMI (weight / height * height)
-	bmi := weight / (height * height)
-	// output the calculated BMI
-	fmt.Printf("Your BMI: %.2f", bmi)
 }
--- info/info.go
+++ info/info.go
@@ -1,8 +1,13 @@
 package info
 
 const (
-	MainTitle    = "BMI Calculator"
-	Seperator    = "--------------------"
-	WeightPrompt = "Please enter your weight (kg): "
-	HeightPrompt = "Please enter your height (m): "
+	MesurmentType = "Type metric for (kg) or english for (lb): "
+	MainTitle     = "BMI Calculator"
+	Seperator     = "--------------------"
+	// Sane rest of world
+	WeightPromptM = "Please enter your weight (kg): "
+	HeightPromptM = "Please enter your height (m): "
+	// US
+	WeightPromptE = "Please enter your weight (lb): "
+	HeightPromptE = "Please enter your height (in): "
 )
